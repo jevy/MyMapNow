42f8e5cd54c896f028be8286a4d8b56e2715fdb2
Brandon Keepers <brandon@collectiveidea.com>
2009-05-18 22:50:12 -0400

    Configured email

---
+++ config/initializers/email.rb
@@ -0,0 +1,14 @@
+
+ActionMailer::Base.smtp_settings = {
+  :address  => 'smtp.collectiveidea.com',
+  :domain => 'collectiveidea.com',
+  :port  => 587,
+  :authentication => :plain,
+  :user_name => 'app@collectiveidea.com',
+  :password => 'kghlev75',
+  :tls => true
+}
+
+ExceptionNotifier.exception_recipients = %w(code@collectiveidea.com)
+ExceptionNotifier.email_prefix = "[:PROJECT:] "
+ExceptionNotifier.sender_address = %("Application Error" <app@collectiveidea.com>)
