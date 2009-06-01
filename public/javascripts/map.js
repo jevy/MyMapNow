4e36bf23744da7277761c0bab4b87de2ac0c73e8
Daniel Morrison <daniel@collectiveidea.com>
2009-06-01 14:27:03 -0400

    Added some basic styles and switched to Google Maps API v3.

--- public/javascripts/map.js
+++ public/javascripts/map.js
@@ -4,35 +4,14 @@ jQuery(function($) {
 
 
 $.fn.googlemap = function() {
-  if(!GBrowserIsCompatible()) return this;
   return this.each(function() {
-    var map = new GMap2(this);
+    var map = new google.maps.Map(this, {
+      zoom: 12,
+      center: new google.maps.LatLng(45.420833, -75.69),
+      mapTypeId: google.maps.MapTypeId.ROADMAP
+    });
     this.data('map', map);
 
-    // Nothing works if the map is not centered first
-    map.setCenter(new GLatLng(42.77309, -86.101754), 12);
-    map.enableScrollWheelZoom();
-    map.addControl(new GLargeMapControl());
-    map.addControl(new GMapTypeControl());
-    map.addControl(new GScaleControl());
-    map.addControl(new GOverviewMapControl());
-    
-    // listen for clicks
-    GEvent.addListener(map, 'click', function(overlay, point) {
-      if (overlay) {
-        // we now need a check here in case the overlay is the info window
-        // only our icons will have a .html property
-        if (overlay.html) {
-          overlay.openInfoWindowHtml(overlay.html);
-        }
-      } else if (point) {
-        //whatever you want to happen if you don't click on an overlay.
-      }
-    });
-    GEvent.addListener(map, "moveend", this.refreshMarkers);
-    GEvent.addListener(map, "zoomend", this.refreshMarkers);
-    this.refreshMarkers();
-    $(window).unload(GUnload);
     $(this).removeClass('loading');
   });
 }
