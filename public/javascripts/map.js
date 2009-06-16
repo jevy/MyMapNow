a23f22f4ff87712da1564490f780366dd8148d5f
Daniel Morrison <daniel@collectiveidea.com>
2009-06-16 16:54:16 -0400

    Use the blue icon on the map and list.

--- public/javascripts/map.js
+++ public/javascripts/map.js
@@ -22,10 +22,14 @@ var Map = {
           new google.maps.Marker({
               position: point, 
               map: Map.map, 
-              title: this.title
+              title: this.title, 
+              icon: new google.maps.MarkerImage("/stylesheets/images/pointer-blue.png",
+                new google.maps.Size(23, 25),
+                new google.maps.Point(0,0),
+                new google.maps.Point(11,20))
           });
           
-          $('<li class="type-1"><h2>' + this.title + '</h2><p class="address">'+this.address+'<p class="description">'+this.description+'</p></li>').appendTo('aside ol');
+          $('<li class="type-1"><div></div><h2>' + this.title + '</h2><p class="address">'+this.address+'<p class="description">'+this.description+'</p></li>').appendTo('aside ol');
         });
       });
     });
