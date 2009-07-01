3efd3ffcee5650425d9170bf8b93775a53f83ce7
Daniel Morrison <daniel@collectiveidea.com>
2009-07-01 09:24:24 -0400

    Updated mongo gem version

--- config/environment.rb
+++ config/environment.rb
@@ -20,7 +20,7 @@ Rails::Initializer.run do |config|
   # config.gem "sqlite3-ruby", :lib => "sqlite3"
   # config.gem "aws-s3", :lib => "aws/s3"
   config.gem 'jnunemaker-validatable', :lib => 'validatable', :source => 'http://gems.github.com'
-  config.gem 'mongodb-mongo', :version => '0.8', :lib => 'mongo', :source => 'http://gems.github.com'
+  config.gem 'mongodb-mongo', :version => '0.9', :lib => 'mongo', :source => 'http://gems.github.com'
 
   # Only load the plugins named here, in the order given (default is alphabetical).
   # :all can be used as a placeholder for all plugins not explicitly named
