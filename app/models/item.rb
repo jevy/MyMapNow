c68d1a84fa6aa90970abae091c291ed71415a168
Steven Baker <steven@stevenrbaker.com>
2009-09-20 15:32:16 -0300

    geocode addresses

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,16 +1,24 @@
 class Item < ActiveRecord::Base
   validates_presence_of :title
   validates_presence_of :begin_at
-  
+
   # FIXME: this is ugly; we really require either lat/lng or address
   validates_presence_of :latitude, :unless => :address_provided?
   validates_presence_of :longitude, :unless => :address_provided?
   validates_presence_of :address, :unless => :latitude_and_longitude_provided?
-  
+
+  after_create :geocode_address
+
+  def geocode_address
+    return unless latitude.nil? or longitude.nil?
+    location = Geocoder.locate(address)
+    self.update_attributes(:latitude => location.latitude, :longitude => location.longitude)
+  end
+
   def address_provided?
     !address.blank?
   end
-  
+
   def latitude_and_longitude_provided?
     !(latitude.nil? and longitude.nil?)
   end
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -9,6 +9,8 @@ describe Item do
       :longitude => -75.693811,
       :address => '24 Sussex Dr., Ottawa, ON, Canada'
     }
+    
+    Geocoder.stub!(:locate => stub(:latitude => 0, :longitude => 0))
   end
 
   it "should create a new instance given valid attributes" do
@@ -39,9 +41,38 @@ describe Item do
     item = Item.create(@valid_attributes.merge(:latitude => nil, :longitude => nil))
     item.should be_valid
   end
-  
+
   it "should not require an address if if lat/lng are provided" do
     item = Item.create(@valid_attributes.merge(:address => nil))
     item.should be_valid
   end
+end
+
+describe "Item Geocoding" do
+  before(:each) do
+    @valid_attributes = {
+      :title => "Prime Minister's Residence",
+      :begin_at => Time.mktime(1983, 3, 22, 15, 35, 0),
+      :latitude => 45.444363,
+      :longitude => -75.693811,
+      :address => '24 Sussex Dr., Ottawa, ON, Canada'
+    }
+  end
+
+  it "should geocode an address if provided" do
+    geocoder = stub(:latitude => @valid_attributes[:latitude], :longitude => @valid_attributes[:longitude])
+    Geocoder.should_receive(:locate).with(@valid_attributes[:address]).and_return(geocoder)
+    item = Item.create(@valid_attributes.merge(:latitude => nil, :longitude => nil))
+    item.latitude.should == @valid_attributes[:latitude]
+    item.longitude.should == @valid_attributes[:longitude]
+  end
+
+  it "should not geocode if latitude and longitude are provided" do
+    Geocoder.should_not_receive(:locate)
+    item = Item.create(@valid_attributes)
+  end
+
+  it "should require valid geocodeable address"
+  it "should require sensible latitude and longitude"
+  it "should possibly reverse geocode (get address for lat/lng)"
 end
