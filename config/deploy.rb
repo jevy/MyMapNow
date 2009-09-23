df46f29fae5a93a9a6212528bd776a8aa2c95c20
Steven Baker <steven@stevenrbaker.com>
2009-09-23 20:43:06 -0300

    deploy task

--- config/deploy.rb
+++ config/deploy.rb
@@ -3,3 +3,10 @@ set :repository, 'gitosis@97.107.135.93:mymapnow.git'
 set :deploy_to, "/home/webapps/#{application}"
 set :web_command, 'sudo apache2ctl'
 set :domain, 'webapps@97.107.135.93'
+
+task :deploy => %w(
+  vlad:update
+  vlad:migrate
+  vlad:start
+  vlad:cleanup
+)
