8df75120cda271803187a09e985f00a96d560aba
Steven Baker <steven@stevenrbaker.com>
2009-09-27 20:49:41 -0300

    disable email for now

--- config/initializers/email.rb
+++ config/initializers/email.rb
@@ -1,14 +1,13 @@
-
-ActionMailer::Base.smtp_settings = {
-  :address  => 'smtp.collectiveidea.com',
-  :domain => 'collectiveidea.com',
-  :port  => 587,
-  :authentication => :plain,
-  :user_name => 'app@collectiveidea.com',
-  :password => 'kghlev75',
-  :tls => true
-}
-
-ExceptionNotifier.exception_recipients = %w(code@collectiveidea.com)
-ExceptionNotifier.email_prefix = "[mymapnow.com] "
-ExceptionNotifier.sender_address = %("Application Error" <app@collectiveidea.com>)
+# ActionMailer::Base.smtp_settings = {
+#   :address  => 'smtp.collectiveidea.com',
+#   :domain => 'collectiveidea.com',
+#   :port  => 587,
+#   :authentication => :plain,
+#   :user_name => 'app@collectiveidea.com',
+#   :password => 'kghlev75',
+#   :tls => true
+# }
+# 
+# ExceptionNotifier.exception_recipients = %w(code@collectiveidea.com)
+# ExceptionNotifier.email_prefix = "[mymapnow.com] "
+# ExceptionNotifier.sender_address = %("Application Error" <app@collectiveidea.com>)
