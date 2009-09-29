ed9af24b025445b8d178539efaf9c786ab73dd54
Steven Baker <steven@stevenrbaker.com>
2009-09-29 20:52:29 -0300

    spread fucking jelly

--- config/environment.rb
+++ config/environment.rb
@@ -20,6 +20,10 @@ Rails::Initializer.run do |config|
   # config.gem "sqlite3-ruby", :lib => "sqlite3"
   # config.gem "aws-s3", :lib => "aws/s3"
   config.gem 'gravtastic', :version => '>= 2.1.0'
+  config.gem 'rspec'
+  config.gem 'cucumber'
+  config.gem 'webrat'
+  config.gem 'jelly'
   
   # Only load the plugins named here, in the order given (default is alphabetical).
   # :all can be used as a placeholder for all plugins not explicitly named
