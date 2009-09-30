0fe24c0c3565cda488e57df2b0278dbb9537013c
Steven Baker <steven@stevenrbaker.com>
2009-09-29 21:12:36 -0300

    fix rspec gem require

--- config/environment.rb
+++ config/environment.rb
@@ -20,7 +20,7 @@ Rails::Initializer.run do |config|
   # config.gem "sqlite3-ruby", :lib => "sqlite3"
   # config.gem "aws-s3", :lib => "aws/s3"
   config.gem 'gravtastic', :version => '>= 2.1.0'
-  config.gem 'rspec'
+  config.gem 'rspec', :lib => 'spec'
   config.gem 'cucumber'
   config.gem 'webrat'
   config.gem 'jelly'
