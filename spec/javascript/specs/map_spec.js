920daed7cba4446e553ae380c1e335530ae88ad1
Steven Baker <steven@stevenrbaker.com>
2009-10-07 00:14:22 -0300

    placeholder for the map spec

---
+++ spec/javascript/specs/map_spec.js
@@ -0,0 +1,7 @@
+describe('Map', function() {
+  it('should load the map at my current location', function() {
+    jasmine.include('public/javascripts/map.js');
+
+    // Map is undefined here!!!
+  });
+});
