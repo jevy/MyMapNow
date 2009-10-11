1db2af49fca23502682beed2ba375762fcc994f7
Jevin Maltais <jevin@crackerJack.(none)>
2009-10-10 21:35:32 -0400

    tried my hand at getting easierness going

--- config/environment.rb
+++ config/environment.rb
@@ -24,6 +24,10 @@ Rails::Initializer.run do |config|
   config.gem 'cucumber'
   config.gem 'webrat'
   config.gem 'jelly'
+  config.gem 'hpricot'
+  config.gem 'fakeweb'
+  config.gem 'nokogiri'
+  config.gem 'selenium-rc'
   
   # Only load the plugins named here, in the order given (default is alphabetical).
   # :all can be used as a placeholder for all plugins not explicitly named
