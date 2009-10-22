bdc2b357f1c4e6900c116021aba39e69b8298f5b
Steven Baker <steven@stevenrbaker.com>
2009-10-21 22:33:25 -0300

    FUCKING TYPO

--- config/environment.rb
+++ config/environment.rb
@@ -1,4 +1,4 @@
-r# Be sure to restart your server when you modify this file
+# Be sure to restart your server when you modify this file
 
 # Specifies gem version of Rails to use when vendor/rails is not present
 RAILS_GEM_VERSION = '2.3.2' unless defined? RAILS_GEM_VERSION
