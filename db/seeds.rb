8d095bbf3184307f38ab15302fc6700c8a08293d
Steven Baker <steven@stevenrbaker.com>
2009-10-25 22:15:14 -0300

    seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,64 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#   
-#   cities = City.create([{ :name => 'Chicago' }, { :name => 'Copenhagen' }])
-#   Major.create(:name => 'Daley', :city => cities.first)
+## Ottawa
+Item.create(:title => 'Eastern Ottawa Economic Showcase',
+            :latitude => 45.397936, :longitude => -75.685518,
+            :begin_at => Date.new(2009, 10, 12))
+Item.create(:title => 'Schmoozefest',
+            :latitude => 45.395674, :longitude => -75.70637,
+            :begin_at => Date.new(2009, 10, 19))
+Item.create(:title => 'Old Hollywood Charity Gala',
+            :latitude => 45.430312, :longitude => -75.698386,
+            :begin_at => Date.new(2009, 10, 16))
+Item.create(:title => 'Beatles Tribute 1964',
+            :latitude => 45.423494, :longitude => -75.697933,
+            :begin_at => Date.new(2009, 10, 15))
+Item.create(:title => 'Oktoberfest',
+            :latitude => 45.342283, :longitude => -75.709856,
+            :begin_at => Date.new(2009, 10, 17))
+Item.create(:title => 'Big Art in the City',
+            :latitude => 45.418365, :longitude => -75.704319,
+            :begin_at => Date.new(2009, 10, 18))
+Item.create(:title => 'A Chorus Line - Broadway Musical',
+            :latitude => 45.423422, :longitude => -75.694797,
+            :begin_at => Date.new(2009, 10, 19))
+Item.create(:title => 'Hike the Mackenzie King Estate',
+            :latitude => 45.487354, :longitude => -75.842725,
+            :begin_at => Date.new(2009, 10, 10))
+Item.create(:title => 'The Great Ottawa Halloween Party',
+            :latitude => 45.423422, :longitude => -75.694797,
+            :begin_at => Date.new(2009, 10, 30))
+Item.create(:title => 'Cultural Showcase',
+            :latitude => 45.380063, :longitude => -75.693435,
+            :begin_at => Date.new(2009, 10, 16),
+            :end_at => Date.new(2009, 10, 17))
+Item.create(:title => 'Ottawa International Animation Festival',
+            :latitude => 45.429355, :longitude => -75.684632,
+            :begin_at => Date.new(2009, 10, 12),
+            :end_at => Date.new(2009, 10, 16))
+Item.create(:title => 'Ottawa Blues Festival',
+            :latitude => 45.430312, :longitude => -75.698386,
+            :begin_at => Date.new(2009, 10, 29),
+            :end_at => Date.new(2009, 11, 1))
+Item.create(:title => 'CKCU Ottawa Folk Festival',
+            :latitude => 45.399761, :longitude => -75.731893,
+            :begin_at => Date.new(2009, 10, 16),
+            :end_at => Date.new(2009, 10, 17))
+
+## Kentville
+Item.create(:title => 'Eastern Ottawa Economic Showcase',
+            :latitude => 45.09814, :longitude => -64.410396,
+            :begin_at => Date.new(2009, 10, 25))
+Item.create(:title => '350 International Day of Climate Action',
+            :latitude => 45.395674, :longitude => -75.70637,
+            :begin_at => Date.new(2009, 10, 24))
+Item.create(:title => 'Acadia Homecoming Weekend',
+            :latitude => 45.091505, :longitude => -64.361687,
+            :begin_at => Date.new(2009, 10, 16),
+            :end_at => Date.new(2009, 10, 19))
+Item.create(:title => 'Horton Annual Craft Fair',
+  :latitude => 45.072142, :longitude => -64.405782,
+            :begin_at => Date.new(2009, 10, 16),
+            :end_at => Date.new(2009, 10, 19))
+Item.create(:title => 'Deep Roots Folk Festival',
+            :latitude => 45.091505, :longitude => -64.361687,
+            :begin_at => Date.new(2009, 10, 23),
+            :end_at => Date.new(2009, 10, 26))
