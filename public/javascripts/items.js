10e04ecc57e6001a2679560bfc8af76dc111340f
Steven Baker <steven@stevenrbaker.com>
2009-10-26 03:12:11 -0300

    use the new bounded finding

--- public/javascripts/items.js
+++ public/javascripts/items.js
@@ -1,15 +1,13 @@
-/*
- {
-        southwest: ''+bounds.getSouthWest().lat()+','+bounds.getSouthWest().lng(),
-        northeast: ''+bounds.getNorthEast().lat()+','+bounds.getNorthEast().lng(),
-        start: $.fn.strftime(timeframe.start, '%Y-%m-%d %H:%M'),
-        end: $.fn.strftime(timeframe.end, '%Y-%m-%d %H:%M')
-      }, 
-      */
-
 $(function() {
   $(document).bind('map:change', function(event, bounds, timeframe) {
-    $.getJSON('/items.js',function(data) {
+    $.getJSON('/items/in_bounds.js',
+	      {
+		southwest: ''+bounds.getSouthWest().lat()+','+bounds.getSouthWest().lng(),
+		northeast: ''+bounds.getNorthEast().lat()+','+bounds.getNorthEast().lng(),
+		start: $.fn.strftime(timeframe.start, '%Y-%m-%d %H:%M'),
+		end: $.fn.strftime(timeframe.end, '%Y-%m-%d %H:%M')
+	      }, 
+	      function(data) {
       // Add new items
       $.each(data, function() {
         Map.addItem(this.item);
