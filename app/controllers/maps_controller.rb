dd8ce3d4e753eba08d45208c9fbc3b5d78fa01bc
Bennett Stevenson <the.ben.stevenson@gmail.com>
2009-11-01 23:37:35 -0700

    Added Connect button to Event Details sidebar messed around with the Facebooker and Facebook Connect API. Next step is to grab the user from the Facebook Session.

--- app/controllers/maps_controller.rb
+++ app/controllers/maps_controller.rb
@@ -1,2 +1,7 @@
 class MapsController < ApplicationController
+
+  def facebook_login  
+    redirect_to :root
+  end
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 ActionController::Routing::Routes.draw do |map|
   map.resources :items, :collection => { :in_bounds => :get }
-
+  map.facebook_login '/maps/facebook_login', :controller=>'maps', :action=>'facebook_login'
   map.root :controller => 'maps'
 end
