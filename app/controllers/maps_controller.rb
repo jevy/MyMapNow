8d90cf8be8edbdb476a7f290969cf54852b55da4
Bennett Stevenson <the.ben.stevenson@gmail.com>
2009-11-12 17:54:51 -0700

    Proper facebooker.yml file and session generation code

--- app/controllers/maps_controller.rb
+++ app/controllers/maps_controller.rb
@@ -1,6 +1,9 @@
 class MapsController < ApplicationController
+  before_filter :set_facebook_session
+  helper_method :facebook_session
 
-  def facebook_login  
+  def facebook_login
+    puts facebook_session.user.events
     redirect_to :root
   end
   
