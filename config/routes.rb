d6ef5b738d410483bc446b1fa91fb78bd5cc8cce
Bennett Stevenson <the.ben.stevenson@gmail.com>
2009-11-21 13:56:33 -0700

    Removal of login name from user, addition of facebook fields. Minor UI stuff. Interim commit.

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,9 @@ ActionController::Routing::Routes.draw do |map|
 
   map.resource :account, :controller => "users"
   map.resources :users
- 
+
+  map.resource :user_sessions
+
   map.resources :items, :collection => { :in_bounds => :get }
   map.facebook_login '/maps/facebook_login', :controller=>'maps', :action=>'facebook_login'
   map.root :controller => 'maps'
