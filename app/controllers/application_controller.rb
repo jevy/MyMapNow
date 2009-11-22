3ab432f24b3f49e7aa2366acb76b5b330aae2b05
Bennett Stevenson <the.ben.stevenson@gmail.com>
2009-11-22 14:55:37 -0700

    Commenting out ANY code not tested in regards to authlogic, won't be back until properly tested.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,22 +6,7 @@ class ApplicationController < ActionController::Base
   helper :all # include all helpers, all the time
   protect_from_forgery # See ActionController::RequestForgeryProtection for details
   filter_parameter_logging :password, :password_confirmation
-  #helper_method :current_user_session, :current_user
-  before_filter :current_user_session
-
-  private
-
-  #Keeps user logged in across sessions.
-  def current_user_session
-    return @current_user_session if defined?(@current_user_session)
-    return false
-    @current_user_session = UserSession.find
-  end
-
-  
-  def current_user
-    return @current_user if defined?(@current_user)
-    @current_user = current_user_session && current_user_session.user
-  end
+#  helper_method :current_user_session
+#  before_filter :current_user_session
 
 end
