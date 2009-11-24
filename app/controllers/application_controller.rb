b7622b92e36422695f80887e24e7b394e822899c
Bennett Stevenson <the.ben.stevenson@gmail.com>
2009-11-23 22:40:04 -0700

    Basic user sessions and blocking of some front-end functionality based on logged in status

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery # See ActionController::RequestForgeryProtection for details
   filter_parameter_logging :password, :password_confirmation
   helper_method :current_user_session
-  #  before_filter :current_user_session
+  before_filter :current_user_session
 
 
   def current_user_session
--- app/controllers/maps_controller.rb
+++ app/controllers/maps_controller.rb
@@ -1,8 +1,2 @@
 class MapsController < ApplicationController
-
-  def facebook_login
-    puts facebook_session.user.events
-    redirect_to :root
-  end
-  
 end
