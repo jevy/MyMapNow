a08660d8a90f1340c066fc90e0871ced8b919a6b
Steven Baker <steven@stevenrbaker.com>
2009-11-25 22:07:18 -0400

    gone; it sucks

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -12,33 +12,6 @@ class ItemsController < ApplicationController
     end
   end
 
-  def in_bounds_for_timeline
-    southwest = params[:southwest].split(',').map(&:to_f)
-    northeast = params[:northeast].split(',').map(&:to_f)
-    begin_at = Time.parse(params[:start])
-    end_at = Time.parse(params[:end])
-
-    @items = Item.find_in_bounds(southwest, northeast, begin_at, end_at)
-
-    the_json = "{ \"events\": ["
-    @items.each do |item|
-      the_json << "{"
-      the_json << "\"start\": new Date(#{item.begin_at.year}, #{item.begin_at.month}, #{item.begin_at.day}, #{item.begin_at.hour}, #{item.begin_at.min}),"
-      the_json << "\"end\": new Date(#{item.end_at.year}, #{item.end_at.month}, #{item.end_at.day}, #{item.end_at.hour}, #{item.end_at.min})," unless item.end_at.blank?
-      the_json << "\"durationEvent\": #{!item.end_at.blank?},"
-      escaped_title = item.title
-      escaped_title = escaped_title.gsub('"', '\"')
-      escaped_title = escaped_title.gsub(',', '\"')
-      the_json << "\"title\": \"#{escaped_title}\""
-      the_json << (item == @items.last ? "}" : "},")
-    end
-    the_json << "]}"
-
-    respond_to do |format|
-      format.js   { render :text => the_json  }
-    end
-  end
-
   # GET /items
   # GET /items.xml
   def index
