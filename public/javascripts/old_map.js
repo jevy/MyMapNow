34e7fbc7e526d661149a12a6f25d2fc95ad8609a
Nancy Blenkhorn <nancy@nancyblenkhorn.com>
2009-12-03 22:46:25 -0400

    css tweakage

--- public/javascripts/old_map.js
+++ public/javascripts/old_map.js
@@ -46,13 +46,13 @@ var OldMap = {
       var point = new google.maps.LatLng(item.latitude, item.longitude);
     
       var $li = $('<li class="'+item.kind+'" data-item-id="'+item.id+'"><div></div></li>').appendTo('aside ol');
-      $li.append('<h2>' + item.title + '</h2>');
+      $li.append('<h2>' + item.title + '</h2><br />');
       $li.append('<p>Start Time: ' + (item.begin_at) + '</p>');
       if (item.end_at) {
-	$li.append('<p>End Time: ' + (item.end_at) + '</p>');
+	$li.append('<p>End Time: ' + (item.end_at) + '<br /></p>');
       }
-      $li.append('<p class="address">' + (item.address || '') + '</p>');
-      $li.append('<p class="description">' + (item.description || '') + '</p>');
+      $li.append('<p class="address">' + (item.address || '') + '<br /></p>');
+      $li.append('<p class="description">' + (item.description || '') + '<br /></p>');
       if (item.url) {
         $li.append('<p class="link"><a href="'+item.url+'" target="_blank">More...</a>');
       }
@@ -68,10 +68,12 @@ var OldMap = {
         Map.showInfoWindow(id);
       });
       google.maps.event.addListener($li.data('marker'), 'mouseover', function() {
-	$('aside li[data-item-id=' + id + ']').css('background', 'lightyellow');
+				$('aside li[data-item-id=' + id + ']').css('background', '#c2ebff');
+				$('aside li[data-item-id=' + id + ']').css('color', '#6e6e6e');
       });
       google.maps.event.addListener($li.data('marker'), 'mouseout', function() {
-	$('aside li[data-item-id=' + id + ']').css('background', '');
+				$('aside li[data-item-id=' + id + ']').css('background', '');
+				$('aside li[data-item-id=' + id + ']').css('color', '');
       });
     }
     
