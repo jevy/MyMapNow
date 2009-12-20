7d82d0248530bdf091d459351533fe003a5206e5
Mike Buckley <wlaver@gmail.com>
2009-12-20 15:19:59 -0700

    Selecting an item, should turn blue.

--- public/javascripts/map.js
+++ public/javascripts/map.js
@@ -1,43 +1,56 @@
 var Map = {
-  initialize: function() {
-    var lat = GeoIP.latitude();
-    var lng = GeoIP.longitude();
-    Map.map = new google.maps.Map($('#map')[0], {
-      zoom: 13,
-      center: new google.maps.LatLng(lat, lng),
-			mapTypeControl: false,
-      mapTypeId: google.maps.MapTypeId.ROADMAP
-    });
-    // FIXME: the next line is untested
-    google.maps.event.addListener(Map.map, 'bounds_changed', function() {
-      Map.fetch();
-    });
-  },
-  updateSearchBoxWithCurrentLocation: function() {
-    $('input[name=search-box]').val(GeoIP.city() + ', ' + GeoIP.region() + ', ' + GeoIP.country());
-  },
-  moveTo: function(location) { // FIXME: untested
-    Map.map.panTo(location);
-  },
-  search: function(query) { // FIXME: the callback is untested
-    var geocoder = new google.maps.Geocoder();
-    geocoder.geocode({ 'address': query }, function(response, status) {
-      if (status == google.maps.GeocoderStatus.OK) {
-				Map.moveTo(response[0].geometry.location);			
-				$('input[name=search-box]').val(response[0].formatted_address);
-      }
-    });
-  },
-	whatsMyLocation: function() { // FIXME: untested
-		Map.updateSearchBoxWithCurrentLocation();
-		Map.search($('#search-box').val());
-	},
-  cleanup: OldMap.cleanup,
-  fetch: OldMap.fetch,
-  showInfoWindow: OldMap.showInfoWindow,
-  addItem: OldMap.addItem,
-  _markerImages: OldMap._markerImages,
-  markerImages: OldMap.markerImages
+    initialize: function() {
+        var lat = GeoIP.latitude();
+        var lng = GeoIP.longitude();
+        Map.map = new google.maps.Map($('#map')[0], {
+            zoom: 13,
+            center: new google.maps.LatLng(lat, lng),
+            mapTypeControl: false,
+            mapTypeId: google.maps.MapTypeId.ROADMAP
+        });
+        // FIXME: the next line is untested
+        google.maps.event.addListener(Map.map, 'bounds_changed', function() {
+            Map.fetch();
+        });
+    },
+    updateSearchBoxWithCurrentLocation: function() {
+        $('input[name=search-box]').val(GeoIP.city() + ', ' + GeoIP.region() + ', ' + GeoIP.country());
+    },
+    moveTo: function(location) { // FIXME: untested
+        Map.map.panTo(location);
+    },
+
+    setMarkerToActiveState: function(marker) {
+        if (marker != undefined) {
+            marker.setIcon("/images/pin_on.png");
+        }
+    },
+
+    setMarkerToDefaultState: function(marker) {
+        if (marker != undefined) {
+            marker.setIcon("/images/pin_off.png");
+        }
+    },
+
+    search: function(query) { // FIXME: the callback is untested
+        var geocoder = new google.maps.Geocoder();
+        geocoder.geocode({ 'address': query }, function(response, status) {
+            if (status == google.maps.GeocoderStatus.OK) {
+                Map.moveTo(response[0].geometry.location);
+                $('input[name=search-box]').val(response[0].formatted_address);
+            }
+        });
+    },
+    whatsMyLocation: function() { // FIXME: untested
+        Map.updateSearchBoxWithCurrentLocation();
+        Map.search($('#search-box').val());
+    },
+    cleanup: OldMap.cleanup,
+    fetch: OldMap.fetch,
+    showInfoWindow: OldMap.showInfoWindow,
+    addItem: OldMap.addItem,
+    _markerImages: OldMap._markerImages,
+    markerImages: OldMap.markerImages
 };
 
 $(document).bind('map:change', Map.cleanup);
--- spec/javascript/specs/helpers/spec_helper_without_caching.js
+++ spec/javascript/specs/helpers/spec_helper_without_caching.js
@@ -1,6 +1,8 @@
 jasmine.include('public/javascripts/jquery/jquery-1.3.2.js', true);
 jasmine.include('spec/helpers/fakes.js', true);
+jasmine.include('http://j.maxmind.com/app/geoip.js', true);
 jasmine.include('public/javascripts/geoip.js', true);
+jasmine.include('http://maps.google.com/maps/api/js?sensor=false', true);
 jasmine.include('public/javascripts/old_map.js', true);
 jasmine.include('public/javascripts/map.js', true);
 jasmine.include('http://static.simile.mit.edu/timeline/api-2.3.0/timeline-api.js?bundle=true', true);
