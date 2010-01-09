e794fce2fe88120b9efa3b165512b178cbff6a4b
Nancy Blenkhorn <nancy@nancyblenkhorn.com>
2010-01-09 18:24:54 -0400

    cleaning up and moving some js

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -2,5 +2,21 @@ $(document).ready(
 	function() {
 		$("ul.tabs").tabs("div.panes > div");
 	}
-	
 );
+
+$(Map.search('Ottawa, ON'));
+
+function submitenter(myfield,e) {
+	var keycode;
+	if (window.event) keycode = window.event.keyCode;
+	else if (e) keycode = e.which;
+	else return true;
+
+	if (keycode == 13)
+	   {
+	   Map.search($('#search-box').val());
+	   return false;
+	   }
+	else
+	   return true;
+}
