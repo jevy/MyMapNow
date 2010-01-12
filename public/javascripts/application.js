1a8a19732bb96e36103989e52f625f90446eef58
Nancy Blenkhorn <nancy@nancyblenkhorn.com>
2010-01-11 23:41:44 -0400

    changing back to the old timeline format

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,22 +1,43 @@
-$(document).ready(
-	function() {
-		$("ul.tabs").tabs("div.panes > div");
-	}
-);
+$(function() {
+  var date = new Date();
+  var startDate = new Date((Math.floor(date.getTime()/(1000 * 60 * 60 * 24))-15)* 1000 * 60 * 60 * 24);
+  var start = 0;
+  var end = (start + 30) * 3;
+  var sliderElement = $('#date-range');
 
-function submitenter(myfield,e) {
-	var keycode;
-	if (window.event) keycode = window.event.keyCode;
-	else if (e) keycode = e.which;
-	else return true;
+  sliderElement.data('start', dateForValue(.25*(end-start)))
+    .data('end', dateForValue(.75*(end-start)))
+    .slider({
+      range: true,
+      min: start,
+      max: end,
+      values: [Math.floor(.25*(end-start)), Math.floor(.75*(end-start))],
+      slide: function(event, ui) {
+	updateLabels(ui.values[0], ui.values[1]);
+      },  
+      change: function(event, ui) {
+	$(this).data('start', dateForValue(ui.values[0]));
+	$(this).data('end', dateForValue(ui.values[1]));
+	Map.fetch();
+      }
+    });
 
-	if (keycode == 13)
-	   {
-	   Map.search($('#search-box').val());
-	   return false;
-	   }
-	else
-	   return true;
-}
+  updateLabels($("#date-range").slider("values", 0), $("#date-range").slider("values", 1));
 
-$(Map.search('Ottawa, ON'));
+  function updateLabels(left, right) {
+    $('#label-left span').text(dateText(left));
+    $('#label-right span').text(dateText(right));
+    setTimeout( function() { $('#label-left').css('right', 100-parseFloat($('#date-range a:first').css('left'))+'%'); }, 10);
+    setTimeout( function() { $('#label-right').css('left', $('#date-range a:last').css('left')); }, 10);
+  }
+
+  function dateForValue(value) {
+    var d = new Date(Math.floor(value/3+(startDate.getTime() / (24 * 60 * 60 * 1000))) * 24 * 60 * 60 * 1000);
+    return d;
+  }
+
+  function dateText(n) {
+    return $.fn.strftime(dateForValue(n), '%B %D');
+  }
+
+});
--- public/javascripts/old_map.js
+++ public/javascripts/old_map.js
@@ -1,125 +1,116 @@
-$('#event-list li').live('click', function(event) {
-    var $this = $(this);
-    $('#event-list li.active').each(function() {
-        $(this).data('info').close();
-        Map.setMarkerToDefaultState($(this).data('marker'));
-    }).removeClass('active');
+$('aside li').live('click', function(event) {
+  var $this = $(this);
+  $('aside li.active').each(function() {
+    $(this).data('info').close();
+  }).removeClass('active');
+  
+  if(!$this.data('info')) {
+    $this.data('info', new google.maps.InfoWindow({
+      content: $this.html(),
+      size: new google.maps.Size(250,150)
+    }));
+  }
 
-    if (!$this.data('info')) {
-        $this.data('info', new google.maps.InfoWindow({
-            content: $this.html(),
-            size: new google.maps.Size(250, 150),
-            disableAutoPan: true
-        }));
-    }
-
-    $this.addClass('active');
-    Map.setMarkerToActiveState($this.data('marker'));
-    Map.map.setCenter($this.data('marker').getPosition());
-    $this.data('info').open(Map.map, $this.data('marker'));
-    MMNTimeline.load_event($this.data('item'));
+  $this.addClass('active');
+  $this.data('info').open(Map.map, $this.data('marker'));
 });
 
 var OldMap = {
-    initialize: function() {
-        Map.map = new google.maps.Map($('#map')[0], {
-            zoom: 13,
-            center: new google.maps.LatLng(45.420833, -75.69),
-            mapTypeId: google.maps.MapTypeId.ROADMAP
-        });
+  initialize: function() {
+    Map.map = new google.maps.Map($('#map')[0], {
+      zoom: 13,
+      center: new google.maps.LatLng(45.420833, -75.69),
+      mapTypeId: google.maps.MapTypeId.ROADMAP
+    });
 
-        $("#date-range").change();
-        google.maps.event.addListener(Map.map, 'bounds_changed', function() {
-            Map.fetch();
-        });
-    },
+    $("#date-range").change();
+    google.maps.event.addListener(Map.map, 'bounds_changed', function() {
+      Map.fetch();
+    });
+  },
+  
+  fetch: function() {
+    var bounds = Map.map.getBounds();
+    var timeframe = {
+      start: $('#date-range').data('start'),
+      end: $('#date-range').data('end')
+    };
+    Map.cleanup();
+    $(document).trigger('map:change', [bounds, timeframe]);
+  },
+  
+  addItem: function(item) {
+    var id = item.id;
+    
+    if(!$('aside li[data-item-id=' + id + ']')[0]) {
+      var point = new google.maps.LatLng(item.latitude, item.longitude);
+    
+      var $li = $('<li class="'+item.kind+'" data-item-id="'+item.id+'"><div></div></li>').appendTo('aside ol');
+      $li.append('<h2>' + item.title + '</h2>');
+      $li.append('<p>Start Time: ' + (item.begin_at) + '</p>');
+      if (item.end_at) {
+	$li.append('<p>End Time: ' + (item.end_at) + '</p>');
+      }
+      $li.append('<p class="address">' + (item.address || '') + '</p>');
+      $li.append('<p class="description">' + (item.description || '') + '</p>');
+      if (item.url) {
+        $li.append('<p class="link"><a href="'+item.url+'" target="_blank">More...</a>');
+      }
 
-    fetch: function() {
-        var bounds = Map.map.getBounds();
-        var timeframe = {
-            start: MMNTimeline.band.getMinVisibleDate(),
-            end: MMNTimeline.band.getMaxVisibleDate()
-        };
-        Map.cleanup();
-        $(document).trigger('map:change', [bounds, timeframe]);
-    },
-
-    addItem: function(item) {
-        var id = item.id;
-
-        if (!$('#event-list li[data-item-id=' + id + ']')[0]) {
-            var point = new google.maps.LatLng(item.latitude, item.longitude);
-
-            var $li = $('<li class="' + item.kind + '" data-item-id="' + item.id + '"></li>').appendTo('#event-list ol');
-            $li.append('<h2>' + item.title + '</h2><br />');
-            $li.append('<p class="time"><br />Start Time: ' + (item.begin_at) + '</p>');
-            if (item.end_at) {
-                $li.append('<p class="time"><br />End Time: ' + (item.end_at) + '</p>');
-            }
-            $li.append('<p class="address"><br />' + (item.address || '') + '</p>');
-            $li.append('<p class="description"><br />' + (item.description || '') + '</p>');
-            if (item.url) {
-                $li.append('<p class="link"><a href="' + item.url + '" target="_blank">More...</a>');
-            }
-
-            $li.data('marker', new google.maps.Marker({
-                position: point,
-                map: Map.map,
-                title: item.title,
-                icon: "images/pin_off.png"
-            }));
-
-            $li.data('item', item);
-
-            google.maps.event.addListener($li.data('marker'), 'click', function() {
-                Map.showInfoWindow(id);
-                MMNTimeline.load_event(item);
-            });
-
-            google.maps.event.addListener($li.data('marker'), 'mouseover', function() {
-                $('#event-list li[data-item-id=' + id + ']').css('background', '#c2ebff');
-                $('#event-list li[data-item-id=' + id + ']').css('color', '#6e6e6e');
-                $('#event-list a').css('color', '#6e6e6e');
-                Map.setMarkerToActiveState($li.data('marker'));
-            });
-            google.maps.event.addListener($li.data('marker'), 'mouseout', function() {
-                $('#event-list li[data-item-id=' + id + ']').css('background', '');
-                $('#event-list li[data-item-id=' + id + ']').css('color', '');
-                $('#event-list a').css('color', '');
-                Map.setMarkerToDefaultState($li.data('marker'));
-            });
-        }
-    },
-
-    showInfoWindow: function(id) {
-        $('#event-list li[data-item-id="' + id + '"]:first').click();
-    },
-
-    highlight: function(item) {
-        Map.map.set_center(new google.maps.LatLng(item.latitude, item.longitude));
-        Map.addItem(item);
-        Map.showInfoWindow(item.id);
-    },
-
-    cleanup: function() {
-        $('#event-list li').each(function() {
-            if ($(this).data('info')) $(this).data('info').close();
-            $(this).data('marker').setMap(null);
-            $(this).remove();
-            MMNTimeline.eventSource.clear();
-        });
-    },
-
-    _markerImages: {},
-    markerImages: function(kind, item) {
-        if (!Map._markerImages[kind]) {
-            var url = item.find('div').css('background-image').match(/\((.*)\)/)[1];
-            var y = item.find('div').css('background-position').match(/-(\d+)/)[1];
-            Map._markerImages[kind] = new google.maps.MarkerImage(url,
-                    new google.maps.Size(23, 25),
-                    new google.maps.Point(0, y),
-                    new google.maps.Point(11, 20));
-        }
-        return Map._markerImages[kind];
+      $li.data('marker', new google.maps.Marker({
+        position: point, 
+        map: Map.map,
+        title: item.title, 
+        icon: Map.markerImages(item.kind, $li)
+      }));
+    
+      google.maps.event.addListener($li.data('marker'), 'click', function() {
+        Map.showInfoWindow(id);
+      });
+      google.maps.event.addListener($li.data('marker'), 'mouseover', function() {
+          $('aside li[data-item-id=' + id + ']').css('background', '#c2ebff');
+          $('aside li[data-item-id=' + id + ']').css('color', '#6e6e6e');
+          $('aside a').css('color', '#6e6e6e');
+          Map.setMarkerToActiveState($li.data('marker'));
+      });
+      google.maps.event.addListener($li.data('marker'), 'mouseout', function() {
+          $('aside li[data-item-id=' + id + ']').css('background', '');
+          $('aside li[data-item-id=' + id + ']').css('color', '');
+          $('aside a').css('color', '');
+          Map.setMarkerToDefaultState($li.data('marker'));
+      });
     }
+  },
+  
+  showInfoWindow: function(id) {
+    $('aside li[data-item-id="'+id+'"]:first').click();
+  },
+  
+  highlight: function(item) {
+    Map.map.set_center(new google.maps.LatLng(item.latitude, item.longitude));
+    Map.addItem(item);
+    Map.showInfoWindow(item.id);
+  },
+  
+  cleanup: function() {
+    $('aside li').each(function() {
+      if($(this).data('info')) $(this).data('info').close();
+      //$(this).data('marker').set_map(null);
+      $(this).remove();
+    });
+  },
+  
+  _markerImages: {},
+  markerImages: function(kind, item) {
+    if (!Map._markerImages[kind]) {
+      var url = item.find('div').css('background-image').match(/\((.*)\)/)[1];
+      var y = item.find('div').css('background-position').match(/-(\d+)/)[1];
+      Map._markerImages[kind] =  new google.maps.MarkerImage(url,
+        new google.maps.Size(23, 25),
+        new google.maps.Point(0, y),
+        new google.maps.Point(0, 0),
+        new google.maps.Point(11,20));
+    }
+    return Map._markerImages[kind];
+  }
 };
