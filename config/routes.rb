28dae7b60c4533a4ccd0f17fd3f05d15627d3c8f
Steven Baker <steven@stevenrbaker.com>
2010-01-25 15:32:58 -0400

    remove the cruft

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,4 @@
 ActionController::Routing::Routes.draw do |map|
-  map.resource :account, :controller => "users"
-  map.resources :users
-  map.resources :user_sessions
-  
   map.resources :items, :collection => { :in_bounds => :get }
-  map.facebook_login '/maps/facebook_login', :controller=>'maps', :action=>'facebook_login'
   map.root :controller => 'maps'
 end
