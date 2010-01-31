b540289c549b693f6a8facb48f71bc1ea1f4056c
Steven Baker <steven@stevenrbaker.com>
2010-01-31 18:34:42 -0400

    add authentication

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 ActionController::Routing::Routes.draw do |map|
   map.resources :items, :collection => { :in_bounds => :get }
+  map.resource :user_session
+  map.resource :account, :controller => "users"
+  map.resources :users
+  
   map.root :controller => 'maps'
 end
