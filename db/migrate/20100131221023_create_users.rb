9df5c17e9921c7e5a6e0cce9ec98d90e1acaed69
Steven Baker <steven@stevenrbaker.com>
2010-01-31 23:03:16 -0400

    drop the users table if it exists

--- db/migrate/20100131221023_create_users.rb
+++ db/migrate/20100131221023_create_users.rb
@@ -1,5 +1,6 @@
 class CreateUsers < ActiveRecord::Migration
   def self.up
+    drop_table :users if table_exists?(:users)
     create_table :users do |t|
       t.string    :email,               :null => false
       t.string    :crypted_password,    :null => false
